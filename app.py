54fb486e432168b936ffac193c9fd0a1fcfa24c9
ryandaryl <ryandarylmills@gmail.com>
2018-02-02 10:57:10 +0000

    Cut down and simplify.

--- app.py
+++ app.py
@@ -1,46 +1,31 @@
 from tornado import websocket, web, ioloop
-import os, json
+import os
 
-cl = []
+client_list = []
 
 class IndexHandler(web.RequestHandler):
     def get(self):
         self.render("index.html")
+        for c in client_list:
+            c.write_message("I'm here.")
 
 class SocketHandler(websocket.WebSocketHandler):
     def check_origin(self, origin):
         return True
 
     def open(self):
-        if self not in cl:
-            cl.append(self)
+        if self not in client_list:
+            client_list.append(self)
 
     def on_close(self):
-        if self in cl:
-            cl.remove(self)
+        if self in client_list:
+            client_list.remove(self)
 
-class ApiHandler(web.RequestHandler):
-
-    @web.asynchronous
-    def get(self, *args):
-        self.finish()
-        id = self.get_argument("id")
-        value = self.get_argument("value")
-        data = {"id": id, "value" : value}
-        data = json.dumps(data)
-        for c in cl:
-            c.write_message(data)
-
-    @web.asynchronous
-    def post(self):
-        pass
 
 app = web.Application([
     (r'/', IndexHandler),
     (r'/ws', SocketHandler),
-    (r'/api', ApiHandler),
     (r'/(favicon.ico)', web.StaticFileHandler, {'path': '../'}),
-    (r'/(rest_api_example.png)', web.StaticFileHandler, {'path': './'}),
 ])
 
 if __name__ == '__main__':
